318596b72d5515f918561387ecb725a5a118a2fc
NAVEEN DHAKED <naveendhaked12@gmail.com>
2022-10-07 02:37:05 +0530

    photos for netlify

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -39,7 +39,7 @@ const Home = () => {
           <img
             src={HeroImage}
             alt="my profile"
-            className="rounded-2xl mx-auto w-2/3 md:w-full"
+            class="p-1 bg-white border rounded max-w-sm"
           />
         </div>
       </div>
