7cab4a4ad290d51e640ffc57fcef1ab6afe1134b
NAVEEN DHAKED <naveendhaked12@gmail.com>
2022-10-07 02:27:40 +0530

    naveen dhaked

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Experience from "./components/Experience";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Portfolio from "./components/Portfolio";
+import SocialLinks from "./components/SocialLinks";
 
 function App() {
   return (
